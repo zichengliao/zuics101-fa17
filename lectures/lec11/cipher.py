8a2b5bf6edde8d6d7558b1fecdcd91c3039b733a
zichengliao <zliao@zju.edu.cn>
2017-10-30 21:58:43 +0800

    f

--- lectures/lec11/cipher.py
+++ lectures/lec11/cipher.py
@@ -1,16 +1,17 @@
 def encode_caesar(message, n):
-	from string import ascii_uppercase as alphabet
-	x = alphabet
-	e = {}
-	for i in range(len(x)):
-		e[x[i]] = x[(i+n)%26]
-	message = message.upper()
-	encoded = ''
-	for c in message:
-		if c in x:
-			encoded += e[c]
-		else:
-			encoded += c
+    from string import ascii_uppercase as alphabet
+    x = alphabet
+    e = {}
+    for i in range(len(x)):
+        e[x[i]] = x[(i+n)%26]
+    
+    message = message.upper()
+    
+    encoded = ''
+    for c in message:
+        if c in x:
+            encoded += e[c]
+        else:
+            encoded += c
 
-	return encoded
-
+    return encoded
